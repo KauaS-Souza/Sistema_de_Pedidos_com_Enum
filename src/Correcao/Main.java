c6f36e0a3b17e0f868c886ab21246eceae8e0c95
KauaS-Souza <kauasilva6h6s@gmail.com>
2026-04-14 11:11:24 -0300

    Adiçaão de i    não de i      dão de i    ninstâcia

--- src/Correcao/Main.java
+++ src/Correcao/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
         Pedido pedido = new Pedido(1, "Bixofis",StatusPedido.AGUARDANDO_PAGAMENTO);
 
+        Pedido pedido2 = new Pedido(2, "Camilly",StatusPedido.AGUARDANDO_PAGAMENTO);
+
+        pedido2.canceladoPedido();
+
         pedido.avancarStatus(pedido);
         IO.println(pedido.getStatus());
 
